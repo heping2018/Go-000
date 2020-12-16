844331323ce21558c9a809c9bc67f752d250f33a
heping2018 <41601722+heping2018@users.noreply.github.com>
2020-12-17 07:54:49 +0800

    Create db.go

--- Week04/demo/internal/dao/db.go
+++ Week04/demo/internal/dao/db.go
@@ -4,3 +4,8 @@ package dao
 func NewDB() (db *sql.DB, cf func(), err error) {
   return
   }
+
+func (d *dao) Query(ctx context.Context, id int64) (art *model.Article, err error) {
+	// get data from db
+	return
+}
